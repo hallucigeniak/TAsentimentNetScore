d5e192af1184ea854cdf7978d10d1d99f8db205c
hallucigeniak <33967043+hallucigeniak@users.noreply.github.com>
2020-03-25 11:42:02 -0600

    Update dbConnect.R

--- Scripts/dbConnect.R
+++ Scripts/dbConnect.R
@@ -5,9 +5,9 @@ library(dplyr)
 
 db_pswd <- Sys.getenv("db_pass")
 db_usr <- "eniak"
-db_hostname <- "192.168.15.115"
+db_hostname <- Sys.getenv("ip_address")
 db_port <- as.numeric(Sys.getenv("db_port"))
-db_name <- "cx_analytics"
+db_name <- Sys.getenv("db_name")
 
 db_con <- dbConnect(MySQL(),
           user = db_usr,
@@ -20,7 +20,7 @@ db_con <- dbConnect(MySQL(),
 #dbWriteTable(db_con, "mtcars_test_1", mtcars)
 
 tabla <- db_con %>% 
-  tbl(in_schema("cx_analytics", "mtcars_test"))
+  tbl(in_schema("db_name", "mtcars_test"))
 
 RMySQL::dbDisconnect(db_con)
   
